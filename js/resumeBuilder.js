a4994a072294458317874f4f6702bcd530b20609
acampos645 <acampos645@gmail.com>
2016-06-30 17:49:56 -0500

    Practicing switching out information

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,22 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+
+
+// [string].replace([old], [new])
+
+// var email = "cameron@udacity.com";
+// var newEmail = email.replace("udacity", "gmail");
+
+// console.log(email);
+// console.log(newEmail);
+
+// var awesomeThoughts = "I am Alex and I am awesome!"
+
+// var funThoughts = awesomeThoughts.replace("awesome", "fun")
+
+// console.log(funThoughts)
+
+// $("#main").append(funThoughts)
+
+var formattedName = HTMLheaderName.replace("%data%", "Alex Alford");
+
+$("#header").append(formattedName)
